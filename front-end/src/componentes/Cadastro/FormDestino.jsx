cbb4b8ada3072ff77b2065d184f263a48408f61b
LesleyOliveira <lesley.oliveira@sptech.school>
2026-09-06 01:03:03 -0300

    formulario pronto

--- front-end/src/componentes/Cadastro/FormDestino.jsx
+++ front-end/src/componentes/Cadastro/FormDestino.jsx
@@ -2,42 +2,136 @@ import { useState } from "react";
 import styles from './FormDestino.module.css';
 
 function FormDestino() {
-    const [nome, setNome] = useState('');
-    const [cidade, setCidade] = useState('');
-    const [pais, setPais] = useState('');
+    const [formulario, setFormulario] = useState({
+        nome: '',
+        cidade: '',
+        pais: '',
+        continente: '',
+        categoria: '',
+        custoEstimado: '',
+        prioridade: '',
+        status: '',
+        observacao: ''
+    })
+
+
+    function alterarCampo(e){
+        const nomeCampo = e.target.name
+        const valor = e.target.value 
+        setFormulario({
+            ...formulario,
+            [nomeCampo]:valor
+        })
+    }
+
 
     return (
-        <section className={styles.containerPrincipal} id="cadastro">
-            <div className={styles.card}>
-                <h2>Cadastre o seu destino</h2>
-                <p className={styles.subtitulo}>Qual lugar do mundo você deseja conhecer?</p>
+        <section className={styles.formulario} id="cadastro">
 
-                <form className={styles.formulario}>
-                    <div className={styles.campo}>
-                        <label>Nome do destino:</label>
-                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} 
-                        />
-                    </div>
+        <div className={styles.card}>
+            <h2 className={styles.titulo}> Cadastre seu destino</h2>
+            <p className={styles.subtitulo}>
+                Qual vai ser o nosso próximo destino?
+            </p>
 
-                    <div className={styles.linhaCampos}>
-                        <div className={styles.campo}>
-                            <label>Cidade</label>
-                            <input type="text" value={cidade} onChange={(e) => setCidade(e.target.value)} 
-                            />
-                        </div>
+            <div className={styles.form}>
+            <div className={styles.campo}>
+                <label htmlFor="">Nome do destino</label>
+                <input type="text"
+                nome="nome"
+                value={formulario.nome}
+                onChange={alterarCampo}
+                />
 
-                        <div className={styles.campo}>
-                            <label>País</label>
-                            <input type="text" value={pais}onChange={(e) => setPais(e.target.value)} 
-                            />
-                        </div>
-                    </div>
-
-                    <button type="button" className={styles.botao}>Cadastrar Destino</button>
-                </form>
             </div>
+
+            <div className={styles.campo}>
+                <label htmlFor="">Cidade</label>
+                <input type="text" name="cidade" value={formulario.cidade}
+                onChange={alterarCampo}
+                 />
+
+            </div>
+
+            <div className={styles.campo}>
+                <label htmlFor="">Pais</label>
+                <input type="text" name="pais" value={formulario.pais} 
+                onChange={alterarCampo}
+                />
+
+            </div>
+
+
+            <div className={styles.campo}>
+                <label htmlFor="">Continente</label>
+                <select name="continente" value={formulario.continente}
+                onChange={alterarCampo}>
+                <option value="">Selecione</option>
+                <option value="AMERICA">América</option>
+                <option value="EUROPA">Europa</option>
+                <option value="ASIA">Ásia</option>
+                <option value="AFRICA">África</option>
+                <option value="OCEANIA">Oceania</option>
+                </select>
+            </div>
+
+
+            <div className={styles.campo}>
+                <label htmlFor="">Custo estimado</label>
+                <input type="number"
+                name="custoEstimado"
+                value={formulario.custoEstimado} 
+                onChange={alterarCampo}/>
+            </div>
+
+            <div className={styles.campo}>
+                <label htmlFor="">Prioridade</label>
+                <select name="prioridade" value={formulario.prioridade} onChange={alterarCampo}>
+                    <option value="">Selecione</option>
+                    <option value="ALTA">Alta</option>
+                    <option value="MEDIA">Media</option>
+                    <option value="BAIXA">Baixa</option>
+                </select>
+            </div>
+
+            <div className={styles.campo}>
+                <label htmlFor="">Status</label>
+                <select name="status" value={formulario.status}
+                onChange={alterarCampo}>
+
+                    <option value="">Selecione</option>
+                    <option value="QUERO_CONHECER">Quero conhecer</option>
+                    <option value="PLANEJANDO">Planejando</option>
+                    <option value="VISITADO">Já visitei</option>
+
+                </select>
+
+            </div>
+
+
+
+            <div className={`${styles.campo} ${styles.campoCompleto}`}> 
+                <label htmlFor="">Observação</label>
+                <textarea name="observacao" value={formulario.observacao} onChange={alterarCampo}/>
+
+            </div>
+
+            <button type="button" className={styles.botao}>Cadastrar destino</button>
+
+
+
+           </div>
+
+
+        </div>
+
+
+
+        
         </section>
-    );
+    )
 }
 
 export default FormDestino;
+
+ 
